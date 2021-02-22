347d0c222c816983b3da77eccd78eabd44612e15
Julian Maldonado <juliandresmc@hotmail.com>
2021-02-22 18:27:37 -0500

    FAB icon grand

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -5,7 +5,7 @@ import AddIcon from '@material-ui/icons/Add';
 export default function BtnAjoutRessource() {
   return (
     <div className="BtnAjoutRessource">
-      <Fab size="small"  aria-label="add" className="fab" >
+      <Fab size="big"  aria-label="add" className="fab" >
           <AddIcon/>
         </Fab>
     </div>
